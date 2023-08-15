6ea3d2457809d39dbe9077dea0782845a7737a43
Daniel Pires <dfpires@gmail.com>
2023-08-15 08:23:42 -0300

    Add files via upload

--- Lista1/src/main/java/lista1/Lista1.java
+++ Lista1/src/main/java/lista1/Lista1.java
@@ -12,31 +12,30 @@ public class Lista1 {
 
     public static void main(String[] args) {
        // instanciar obj1
-       Produto obj1 = new Produto();
-       // instanciar obj2
-       Produto obj2 = new Produto();
+       Produto obj1 = new Produto(1, "Sabonete", 
+               "Dove Creme", 10, 1.8f);
+        
+       Produto obj2 = new Produto(2, "Sabão", "Neutro", 
+                20, 1.2f);
        
-       // adiciona valores para obj1
-	obj1.codigo = 1;
-	obj1.nome = "Sabonete";
-	obj1.descricao = "Dove Creme";
-	obj1.qtde = 10;
-	obj1.preco = 1.8f;
-       // adiciona valores para obj2
-       obj2.codigo = 2;
-       obj2.nome = "Sabão";
-       obj2.descricao = "Neutro";
-       obj2.qtde = 20;
-       obj2.preco = 1.2f;
-       
-       // exibir os dados de obj1
-       System.out.println("Código " + obj1.codigo + " Nome " + obj1.nome + 
-               " Descrição " + obj1.descricao + " Qtde " + obj1.qtde + " Preço " + 
-               obj1.preco);
-       
-       // exibir os dados de obj2
-       System.out.println("Código " + obj2.codigo + " Nome " + obj2.nome + 
-               " Descrição " + obj2.descricao + " Qtde " + obj2.qtde + " Preço " + 
-               obj2.preco);
+      // exibe os dados de obj1
+      obj1.exibeDados();
+      
+      // exibe os dados de obj2
+      obj2.exibeDados();
+      
+      obj1.comprar(5);
+      obj2.comprar(8);
+      
+      obj1.exibeDados();
+      obj2.exibeDados();
+      
+      obj1.vender(2);
+      obj2.vender(5);
+      
+      obj1.exibeDados();
+      obj2.exibeDados();
+      
+      
     }
 }
--- Lista1/src/main/java/lista1/Produto.java
+++ Lista1/src/main/java/lista1/Produto.java
@@ -10,4 +10,36 @@ public class Produto {
     public String descricao;
     public int qtde;
     public float preco;
+    
+    // método construtor, tem o mesmo nome da classe
+    public Produto(int codigo, String nome, String descricao, 
+            int qtde, float preco){
+        // this.codigo representa o código do obj que chama construtor
+        // codigo representa o valor informado pelo usuário
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.qtde = qtde;
+        this.preco = preco;
+    }
+    // comprar produtos
+    public void comprar(int x){
+        this.qtde = this.qtde + x;
+    }
+    
+    // vender produto
+    public void vender(int x){
+        if (x <= this.qtde){
+            this.qtde = this.qtde - x;
+        }
+        else {
+            System.out.println("Estoque insuficiente");
+        }
+    }
+    public void exibeDados(){
+         // exibir os dados do objeto que chama o método
+       System.out.println("Código " + this.codigo + " Nome " + this.nome + 
+               " Descrição " + this.descricao + " Qtde " + this.qtde + " Preço " + 
+               this.preco);
+    }
 }

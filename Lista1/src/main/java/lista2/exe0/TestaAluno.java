1e204e77562f3097bbcd30a560e781dca103b8e8
Daniel Pires <dfpires@gmail.com>
2023-08-22 08:59:33 -0300

    Add files via upload

---
+++ Lista1/src/main/java/lista2/exe0/TestaAluno.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lista2.exe0;
+
+/**
+ *
+ * @author danie
+ */
+public class TestaAluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Aluno obj1 = new Aluno();
+        obj1.setIdade(-18);
+        obj1.ra = 123;
+        obj1.p1 = -4;
+        obj1.p2 = -5;
+        
+        Aluno obj2 = new Aluno();
+        obj2.setIdade(18);
+        obj2.ra = 456;
+        obj2.p1 = 6.5f;
+        obj2.p2 = 8.5f;
+    }
+    
+}

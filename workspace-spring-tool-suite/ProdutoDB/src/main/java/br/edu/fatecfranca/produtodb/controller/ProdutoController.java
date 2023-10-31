adaf8aec614b17f4cbe9f556d0d800617dcfd986
Daniel Pires <dfpires@gmail.com>
2023-10-31 07:58:41 -0300

    Add files via upload

--- workspace-spring-tool-suite/ProdutoDB/src/main/java/br/edu/fatecfranca/produtodb/controller/ProdutoController.java
+++ workspace-spring-tool-suite/ProdutoDB/src/main/java/br/edu/fatecfranca/produtodb/controller/ProdutoController.java
@@ -1,10 +1,14 @@
 package br.edu.fatecfranca.produtodb.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,10 +33,35 @@ public class ProdutoController {
 		return injecao.findAll();
 	}
 	
+	// consulta no banco de dados por um produto em específico
+	@GetMapping("/{id}")
+	public Optional<Produto> getProduto(@PathVariable Long id) {
+		Optional<Produto> prod =  injecao.findById(id);
+		return prod;
+	}
+	
+	
 	// insere no banco de dados é sempre o verbo POST
 	// recupera o corpo da requisição e insere no banco
 	@PostMapping
 	public Produto addProduto(@RequestBody Produto produto) {
+		return injecao.save(produto);
+	}
+	
+	// remove do banco de dados é sempre o verbo DELETE
+	@DeleteMapping("/{id}")
+	public String removeProduto(@PathVariable Long id) {
+		injecao.deleteById(id);
+		return "Remoção com sucesso";
+	}
+	
+	// atualiza no banco de dados é sempre com PUT
+	@PutMapping()
+	public Produto updateProduto(@RequestBody Produto produto) {
+		// perceba que o save está sendo utilizado na inserção
+		// e atualização. Isso porque, caso o produto não
+		// tenha id, é pra salvar, caso o produto tenha id,
+		// vai atualizar
 		return injecao.save(produto);
 	}
 }
